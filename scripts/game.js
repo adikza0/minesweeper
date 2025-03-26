f9036addc969e2f527dfddcf7c843ccb3421e0a0
adikza0 <adikzak@seznam.cz>
2025-03-26 22:27:03 +0100

    generateHTML of matrix and added tests for it

--- scripts/game.js
+++ scripts/game.js
@@ -1,23 +1,24 @@
 import { Matrix } from './Matrix.js';
 
-const matrix = new Matrix(5, 10, 5);
+const testMatrix = new Matrix(5, 10, 5);
+generateHTML(testMatrix);
 
-generateHTML();
-function generateHTML() {
+
+export function generateHTML(matrix) {
   let gameHTML = '';
   for (let i = 0; i < matrix.rows; i++) {
     gameHTML += '<div class="row">';
     for (let j = 0; j < matrix.columns; j++) {
       let content = '';
-      //if (matrix.matrix[i][j].isRevealed) {
+      if (matrix.matrix[i][j].isRevealed) {
         if (matrix.matrix[i][j].isMine) {
           content = '💣';
         }
         else {
           content = matrix.matrix[i][j].adjacentMines;
         }
-      //}
-      gameHTML += `<div class="cell revealed" data-x="${j}" data-y="${i}">${content}</div>`;
+      }
+      gameHTML += `<div class="cell" data-x="${j}" data-y="${i}">${content}</div>`;
     }
     gameHTML += '</div>';
   }
